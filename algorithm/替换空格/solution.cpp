70014e7a90c72a1a129998ed0df12a3019388c26
Vccccc <Vccccc>
2019-05-03 21:02:49 +0800

    update

--- algorithm/替换空格/solution.cpp
+++ algorithm/替换空格/solution.cpp
@@ -33,3 +33,24 @@ void replaceSpace(string& str)
     }
     str = res;
 }
+
+int main()
+{
+    string test("We are happy.");
+    string test1(" We are happy.");
+    string test2("We are happy. ");
+    string test3("We   are  happy.");
+    string test4("          ");
+    
+    replaceSpace(test);
+    replaceSpace(test1);
+    replaceSpace(test2);
+    replaceSpace(test3);
+    replaceSpace(test4);
+    cout << test << "\n";
+    cout << test1 << "\n";
+    cout << test2 << "\n";
+    cout << test3 << "\n";
+    cout << test4 << "\n";
+    return 0;
+}
