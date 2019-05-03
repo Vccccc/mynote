760f9d3c1e7c2a07e9e6651b517c5fdddc916f4a
Vccccc <Vccccc>
2019-05-03 20:51:49 +0800

    update

---
+++ algorithm/替换空格/solution.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+void replaceSpace(string& str)
+{
+    int n = 0;
+    for(auto& i : str)
+    {
+        if(i == ' ')
+        {
+            ++n;
+        }
+    }
+    
+    n *= 2;
+    n += str.size();
+    string res(n);
+
+    for(int i = str.size() - 1, j = n - 1; i >= 0;)
+    {
+        if(str[i] != ' ') 
+        {
+            continue;
+        }
+        else
+        {
+            res[j--] = '0';
+            res[j--] = '2';
+            res[j--] = '%';
+        }
+    }
+}
